83fed96c77fd4eed1f9e7d8838ca0aedd50afc22
Sam Cook <sam.cook@suusuccess.org>
2020-11-19 11:51:02 -0700

    created a post component

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -4,10 +4,28 @@ import { connect } from 'react-redux';
 
 import * as actions from '../actions';
 
+import Post from './post';
+
 class RecentPosts extends Component {
 
     componentDidMount() {
         this.props.fetchRecentPosts();
+    }
+
+    renderPosts = function() {
+        const posts= this.props.recentPosts.map((post, index) => {
+            
+            if(index < 3) {
+                return ( 
+                    <li>
+                        <Post {...post} key={index} />
+                        </li>
+                    )
+            }
+
+           
+        })
+        return posts
     }
 
     render() {
@@ -18,9 +36,7 @@ class RecentPosts extends Component {
                    <div className="recent-posts-heading">Recent Posts</div>
 
                    <ul className="recent-posts-posts">
-                        <li> Recent posts 0 </li>
-                        <li> Recent posts 1 </li>
-                        <li> Recent posts 2</li>
+                        {this.renderPosts()}
                    </ul>
 
                </div>
@@ -30,4 +46,10 @@ class RecentPosts extends Component {
     }
 }
 
-export default connect(null, actions)(RecentPosts); 
+function mapStateToProps(state) {
+    return {
+        recentPosts: state.posts.recentPosts
+    }
+}
+
+export default connect(mapStateToProps, actions)(RecentPosts); 
